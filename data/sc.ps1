af4c30259f9686b8afc3739d049e2e0009104e16
Muhammad Zain <134594267+zainmustafam977@users.noreply.github.com>
2025-05-31 22:00:20 +0500

    sc.ps1

--- data/sc.ps1
+++ data/sc.ps1
@@ -1,5 +1,5 @@
 
 $authKey = $env:TAILSCALE_AUTH_KEY
 #tailscale up --auth-key=tskey-auth-kn8TU1m4GE11CNTRL-RWL3PgUvAtGbFetLC6N8tG7RLQkk6hSy --unattended
-tailscale up --authkey=$authKey
+tailscale up --authkey $authKey
 tailscale status
