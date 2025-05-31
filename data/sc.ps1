21a7705aa657aaa5ff8b22cc947778218a655f6c
Muhammad Zain <134594267+zainmustafam977@users.noreply.github.com>
2025-05-31 22:10:30 +0500

    sc.ps1

--- data/sc.ps1
+++ data/sc.ps1
@@ -1,5 +1,5 @@
 
-$authKey = $env:TAILSCALE_AUTH_KEY
+$authKey = "tskey-auth-k4C5uZBb9g11CNTRL-u6sSeAruD9WPFuC4nMBL9WGAQsguN7T1"
 Write-Host $authKey
 tailscale up --authkey $env:TAILSCALE_AUTH_KEY --verbose
 tailscale status
